1ebe2840724087c55291ac6a5dbe6dc3926ed33b
moataz osama <moataz.osama@hotmail.com>
2020-03-15 13:38:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-add()
 function makeInt(string,)
 {
   return  parseInt(string,10)
@@ -8,3 +7,6 @@ function preserveDecimal(string)
 {
   return parseFloat(string)
   }
+  function myFunction(p1, p2) {
+  return p1 + p2;
+}
