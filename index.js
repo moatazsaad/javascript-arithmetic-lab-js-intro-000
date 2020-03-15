012f1a792dfc1a9f83fccf09e21348da9acdb2f9
moataz osama <moataz.osama@hotmail.com>
2020-03-15 13:43:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function subtract (a, b)
 {
 return a - b
 }
-function multiplies (a, b)
+function multiply (a, b)
 {
 return a * b
 }
-function divides (a, b)
+function divide (a, b)
 {
 return a / b
 }
