a2bf26f2ba6c17a6b49262a584590f36bb85e415
moataz osama <moataz.osama@hotmail.com>
2020-03-15 13:42:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ function preserveDecimal(string)
   }
   function add (a, b)
   {
-  return a + b;
+  return a + b
 }
 function subtract (a, b)
 {
-return a - b;
+return a - b
 }
 function multiplies (a, b)
 {
-return a * b;
+return a * b
 }
 function divides (a, b)
 {
-return a / b;
+return a / b
 }
